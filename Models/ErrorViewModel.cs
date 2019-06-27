54c47fb77719f10154650f5fc8b776d8d4c66b07
nhhamza <hamzalhimer230@gmail.com>
2019-06-27 09:01:35 +0200

    first approach

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace empty_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
